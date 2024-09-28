959b6b7e33b7dbb8e1597debd2f55355bbfc2b42
Eyobed-Tezera <eyobed014@gmail.com>
2024-09-28 10:25:57 -0700

    netlify

--- AMAZON-CLON/src/Api/axios.js
+++ AMAZON-CLON/src/Api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-ec584/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-ec584/us-central1/api",
+  baseURL: "https://api-bpkgyk2m7a-uc.a.run.app",
 });
 export default axiosInstance;
